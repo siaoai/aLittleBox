63cddf1a625de39d51ddef43369d415d581b7aa4
siaoai <56486887+siaoai@users.noreply.github.com>
2019-10-13 13:34:09 +0800

    Delete setup.cmd

--- setup.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem FiveM Bot Setup (NodeJS)
-echo FiveM Bot Setup (NodeJS)
-echo Make sure to run both of the following prompts
-PAUSE
-cd modules/scripts
- start i.bat
- start stocks.bat
-EXIT
